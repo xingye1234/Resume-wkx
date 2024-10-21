4e67dcd33d6bf7f2c83739350ef8edf00ec8ce4b
wangkexing <1989163733@qq.com>
2024-10-21 13:10:11 +0800

    add base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { loadEnv } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [react()],
+    base: env.NODE_ENV === "production" ? "/Resume-wkx/" : "",
+  };
+});
