3cfd7bfc5f78baebf71b44802885d902745ba8f9
wangkexing <1989163733@qq.com>
2026-05-07 18:04:18 +0800

    fix:update useTheme hooks

--- src/hooks/useThemes.ts
+++ src/hooks/useThemes.ts
@@ -1,8 +1,13 @@
 import { useState } from "react";
-import { getLocalStorage } from "../utils/storage";
+import { getLocalStorage, setLocalStorage } from "../utils/storage";
 
 export const useThemes = () => {
   const [theme, setTheme] = useState<string>(getLocalStorage("theme") || "light");
 
-  return { theme, setTheme };
+  const setThemeAndStorage = () => {
+    setTheme(pre => pre === "light" ? "dark" : "light");
+    setLocalStorage("theme", theme === "light" ? "dark" : "light");
+  };
+
+  return { theme, setTheme: setThemeAndStorage };
 };
